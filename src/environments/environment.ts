3fb902ef115c08a81bdec58fc515de54d00eaac0
Shashank Bharide <shashankbharide@cgi.com>
2024-12-11 23:06:20 -0500

    cahnages to environment.dev.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8001/finplay/api'
+    apiUrl: 'http://finplay-external-alb-1765525093.us-east-1.elb.amazonaws.com/api/spring/finplay/api'
 }
